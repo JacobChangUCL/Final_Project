fc60b47719fd566cc4b2019f66f38e2e7b0c61a7
Jingbo Zhang <uceehbb@ucl.ac.uk>
2025-02-12 23:10:09 +0800

    To make grading easier, I've included my API key in the config file

--- src/ClinicInteract/cofig.py
+++ src/ClinicInteract/cofig.py
@@ -1,7 +1,7 @@
 
-class Config:
-    openai_api_key=""
-    deepseek_api_key=""
-    ali_yun_bai_lian_api_key=""
-    silicon_flow=""
+class Config:    
+    openai_api_key="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"
+    deepseek_api_key="sk-2723f21ed00148bf971d4d5160ca3b31"
+    ali_yun_bai_lian_api_key="sk-c355f40452c742048019a4fb5e2fddbe"
+    silicon_flow="Bearer sk-mssyznxctiiampycqniqtkzahxelihvvnczoxopemxxmicse"
     #silicon_flow is an LLM inference provider.
