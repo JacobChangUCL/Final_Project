0073d660a5c95383f5e9973e547aae0aa838b862
JacobChangUCL <uceehbb@ucl.ac.uk>
2025-03-19 20:21:07 +0800

    Update cofig.py
    Received an abnormal API usage bill; the API key may have been leaked.

--- src/ClinicInteract/cofig.py
+++ src/ClinicInteract/cofig.py
@@ -1,7 +1,7 @@
 
 class Config:    
-    openai_api_key="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"
-    deepseek_api_key="sk-2723f21ed00148bf971d4d5160ca3b31"
-    ali_yun_bai_lian_api_key="sk-c355f40452c742048019a4fb5e2fddbe"
-    silicon_flow="Bearer sk-mssyznxctiiampycqniqtkzahxelihvvnczoxopemxxmicse"
+    openai_api_key="YourOpenAiApiKey"
+    deepseek_api_key="Provided by the official Deepseek website."
+    ali_yun_bai_lian_api_key="https://www.aliyun.com/product/bailian"
+    silicon_flow="YourDeepseekAPIKey,preference SiliconFlow Inference Provider"
     #silicon_flow is an LLM inference provider.
